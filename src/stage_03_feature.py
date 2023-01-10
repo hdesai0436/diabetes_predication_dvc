c6bda526243a682187a641d68009af67b77bc761
Hardik0436@ <hardikdesai262@gmail.com>
2023-01-10 17:39:21 -0500

    feature_selection

---
+++ src/stage_03_feature.py
@@ -0,0 +1,40 @@
+import argparse
+import pandas as pd
+from src.utils.all_utils import read_yaml,create_dir,save_local_df,feature_selection
+import os
+import numpy as np
+from sklearn.feature_selection import SelectKBest,mutual_info_classif
+from sklearn.feature_selection import chi2
+
+
+def feature_selection_df(config_path):
+    config = read_yaml(config_path)
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    clean_data_dir = config['artifacts']['clean_data_dir']
+    fill_missing_value_file = config['artifacts']['fill_missing_value_df']
+
+    # get filling missing data
+    fill_missing_df_path = os.path.join(artifacts_dir,clean_data_dir,fill_missing_value_file)
+    df = pd.read_csv(fill_missing_df_path)
+    feature_df, feature_important_score = feature_selection(df)
+    #feature selection file
+    feature_selection_dir = config['artifacts']['feature_selection_dir']
+    feature_selection_score_path = config['artifacts']['feature_selection_score']
+    feature_selection_df_path = config['artifacts']['after_selection_data']
+
+    feature_selection_dir_path = os.path.join(artifacts_dir,feature_selection_dir)
+    create_dir([feature_selection_dir_path])
+    feature_important_file_path = os.path.join(feature_selection_dir_path,feature_selection_score_path)
+    feature_selection_df_file = os.path.join(feature_selection_dir_path,feature_selection_df_path)
+    save_local_df(feature_important_score,feature_important_file_path)
+    save_local_df(feature_df,feature_selection_df_file)
+
+    
+
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config/config.yaml")
+    parsed_arg = args.parse_args()
+    feature_selection_df(parsed_arg.config)
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,6 +1,10 @@
 import yaml
 import os
 import numpy as np
+from sklearn.feature_selection import SelectKBest,mutual_info_classif
+from sklearn.feature_selection import chi2
+import pandas as pd
+import joblib
 
 def read_yaml(path_to_yaml:str):
     with open(path_to_yaml) as yaml_file:
@@ -26,3 +30,20 @@ def fill_missing_value(dataframe):
         df.loc[(df['Outcome'] == 0 ) & (df[i].isnull()), i] = temp[i].iloc[0]
         df.loc[(df['Outcome'] == 1 ) & (df[i].isnull()), i] = temp[i].iloc[1]
     return df
+
+
+def feature_selection(data):
+    df = data
+    X = df.drop('Outcome',axis=1)
+    y = df['Outcome']
+    ordered_rank_features=SelectKBest(score_func=chi2,k=6)
+    ordered_feature=ordered_rank_features.fit(X,y)
+    dfscores=pd.DataFrame(ordered_feature.scores_,columns=["Score"])
+    dfcolumns=pd.DataFrame(X.columns)
+    features_rank=pd.concat([dfcolumns,dfscores],axis=1)
+    features_rank.columns=['Features','Score']
+    best_feature_6 = features_rank.nlargest(6,'Score')['Features']
+    col_name = list(best_feature_6) # convert pandas series to list 
+    col_name.append('Outcome') # append target columns to list
+    feature_df = df.loc[:, df.columns.isin(list(col_name))] # choose only select columns from pandas dataframe
+    return feature_df, features_rank
